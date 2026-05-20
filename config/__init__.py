e2f5478336c6178c3e56a6fb203bde95ec9cb14c
Ankakna <1363168653@qq.com>
2026-05-20 18:40:15 +0800

    feat:更新时装数据

--- config/__init__.py
+++ config/__init__.py
@@ -2,5 +2,6 @@ from .arm_map import CATEGORY_MAP
 from .skill_rename_map import SKILL_RENAME_MAP
 from .skill_category_map import SKILL_CATEGORY_MAP
 from .suit_map import GATHER_SUIT_MAP
+from .fashion_config import normalize_fashion_cn
 
 from .body_rename_map import BODY_RENAME_MAP
